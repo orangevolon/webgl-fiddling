ca7dcdb7196ae38555c6ac7bcc226b3b2c94d2cd
Amir Alami <abstract.stream@gmail.com>
2023-05-14 18:21:00 +0200

    chore: Indices and positions merged into a single buffer creator.

--- src/createModel.js
+++ src/createModel.js
@@ -1,41 +1,70 @@
 import { vec3 } from "gl-matrix";
 import { rotate, translate } from "./transforms";
 
-export function createSquare(width) {
-  return [
-    [-width / 2, -width / 2, 0],
-    [width / 2, -width / 2, 0],
-    [width / 2, width / 2, 0],
-    [-width / 2, width / 2, 0],
-  ];
-}
-
-export function createModel() {
-  const [top, left, right, bottom, front, back] = Array(6)
+function createBox(width) {
+  const faces = Array(6)
     .fill()
-    .map(() => createSquare(1));
+    .map(() => [
+      [-width / 2, -width / 2, 0],
+      [width / 2, -width / 2, 0],
+      [width / 2, width / 2, 0],
+      [-width / 2, width / 2, 0],
+    ]);
+
+  const [top, left, right, bottom, front, back] = faces;
 
   // Top
-  translate(top, { x: 0, y: 0, z: 1 });
+  translate(top, { x: 0, y: 0, z: width / 2 });
 
   // Bottom
-  translate(bottom, { x: 0, y: 0, z: -1 });
+  translate(bottom, { x: 0, y: 0, z: -width / 2 });
 
   // Left
   rotate(left, { rx: 0, ry: Math.PI / 2, rz: 0 });
-  translate(left, { x: -1, y: 0, z: 0 });
+  translate(left, { x: -width / 2, y: 0, z: 0 });
 
   // Right
   rotate(right, { rx: 0, ry: Math.PI / 2, rz: 0 });
-  translate(right, { x: 1, y: 0, z: 0 });
+  translate(right, { x: width / 2, y: 0, z: 0 });
 
   // Front
   rotate(front, { rx: Math.PI / 2, ry: 0, rz: 0 });
-  translate(front, { x: 0, y: 1, z: 0 });
+  translate(front, { x: 0, y: width / 2, z: 0 });
 
   // Back
   rotate(back, { rx: Math.PI / 2, ry: 0, rz: 0 });
-  translate(back, { x: 0, y: -1, z: 0 });
+  translate(back, { x: 0, y: -width / 2, z: 0 });
 
-  return [top, left, right, bottom, front, back].flat(2);
+  const indices = faces.flatMap((_, index) => {
+    const base = index * 4;
+    const sideIndices = [
+      // first triangle
+      0, 1, 2,
+      // second triangle
+      0, 2, 3,
+    ];
+    return sideIndices.map((i) => i + base);
+  });
+
+  const vertices = faces.flat();
+  const positions = vertices.flat();
+
+  return { positions, indices };
+}
+
+function createSphere(radius) {
+  // do nothing yet
+}
+
+export function createModel(params) {
+  const { type, ...modelProps } = params;
+
+  switch (type) {
+    case "box":
+      return createBox(modelProps.width);
+    case "sphere":
+      return createSphere(modelProps.radius);
+    default:
+      throw new Error(`Unknown model type: ${type}`);
+  }
 }
--- src/initBuffers.js
+++ src/initBuffers.js
@@ -1,10 +1,9 @@
 import { createModel } from "./createModel";
 
 function initBuffers(gl) {
-  const positionBuffer = initPositionBuffer(gl);
+  const { positionBuffer, indexBuffer } = initVerticesBuffer(gl);
   const colorBuffer = initColorBuffer(gl);
   const normalBuffer = initNormalBuffer(gl);
-  const indexBuffer = initIndexBuffer(gl);
 
   return {
     position: positionBuffer,
@@ -14,84 +13,36 @@ function initBuffers(gl) {
   };
 }
 
-function initPositionBuffer(gl) {
-  // Create a buffer for the square's positions.
+function initVerticesBuffer(gl) {
+  // Create buffers
   const positionBuffer = gl.createBuffer();
+  const indexBuffer = gl.createBuffer();
 
   // Select the positionBuffer as the one to apply buffer
   // operations to from here out.
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
 
-  // Now create an array of positions for the square.
-  const positions = createModel();
+  // Now create model positions and indices
+  const { positions, indices } = createModel({
+    type: "box",
+    width: 1,
+  });
 
   // Now pass the list of positions into WebGL to build the
   // shape. We do this by creating a Float32Array from the
   // JavaScript array, then use it to fill the current buffer.
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-  return positionBuffer;
-}
-
-export { initBuffers };
-
-function initIndexBuffer(gl) {
-  const indexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-  // This array defines each face as two triangles, using the
-  // indices into the vertex array to specify each triangle's
-  // position.
-
-  const indices = [
-    0,
-    1,
-    2,
-    0,
-    2,
-    3, // front
-    4,
-    5,
-    6,
-    4,
-    6,
-    7, // back
-    8,
-    9,
-    10,
-    8,
-    10,
-    11, // top
-    12,
-    13,
-    14,
-    12,
-    14,
-    15, // bottom
-    16,
-    17,
-    18,
-    16,
-    18,
-    19, // right
-    20,
-    21,
-    22,
-    20,
-    22,
-    23, // left
-  ];
-
-  // Now send the element array to GL
-
   gl.bufferData(
     gl.ELEMENT_ARRAY_BUFFER,
     new Uint16Array(indices),
     gl.STATIC_DRAW
   );
 
-  return indexBuffer;
+  return { positionBuffer, indexBuffer };
 }
+
+export { initBuffers };
 
 function initColorBuffer(gl) {
   const colors = [
